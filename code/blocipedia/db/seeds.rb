961870a73faf2b4a61be419876148e4bf42152bb
boumsam <sam_boumeester41@hotmail.com>
2016-11-03 00:30:33 +0000

    seedingData: completed

--- code/blocipedia/db/seeds.rb
+++ code/blocipedia/db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+3.times do
+    User.create!(
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        password: Faker::Internet.password,
+        role: "admin"
+    )
+end
+
+5.times do
+    User.create!(
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        password: Faker::Internet.password,
+        role: "premium"
+    )
+end
+
+5.times do
+    User.create!(
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        password: Faker::Internet.password,
+        role: "standard"
+    )
+end
+
+users = User.all
+
+25.times do
+    Wiki.create!(
+        user: users.sample,
+        title: Faker::Lorem.sentence,
+        body: Faker::Lorem.paragraph
+    )
+end
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
+
+    
